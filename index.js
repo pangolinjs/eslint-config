7c28411bfad6ba226bce24c46a00a37ae2d66617
Fynn Becker <post@fynn.be>
2022-11-12 09:38:17 +0100

    feat: Indent with tabs
    BREAKING CHANGE: Indentation with spaces will cause an error.

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  extends: [
-    require.resolve('eslint-config-standard')
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  env: {
-    browser: true
-  },
-  rules: {
-    'no-console': 'error',
-    'no-restricted-globals': [
-      'error',
-      {
-        name: 'name',
-        message: 'Use local parameter instead.'
-      }
-    ],
-    'no-warning-comments': 'warn'
-  }
+	root: true,
+	extends: [
+		require.resolve('eslint-config-standard')
+	],
+	parserOptions: {
+		ecmaVersion: 'latest'
+	},
+	env: {
+		browser: true
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'no-console': 'error',
+		'no-restricted-globals': [
+			'error',
+			{
+				name: 'name',
+				message: 'Use local parameter instead.'
+			}
+		],
+		'no-tabs': 'off',
+		'no-warning-comments': 'warn'
+	}
 }
